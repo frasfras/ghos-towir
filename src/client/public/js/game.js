9670829124ef03dbf785b543e4d018a996405692
frasfras <fsilvbo@gmail.com>
2025-10-29 23:43:36 +0800

    update logo

--- src/client/public/js/game.js
+++ src/client/public/js/game.js
@@ -14,10 +14,10 @@ var HEIGHT;
     // canvas.height = HEIGHT;
 
     window.oMain = new CMain({
-        stack_velocity: 15, //SUBTRACT THIS SCORE EVERY FAILED LAUNCH
+        stack_velocity: 15, 
         audio_enable_on_startup:true, //ENABLE/DISABLE AUDIO WHEN GAME STARTS 
         fullscreen: false, 
-        check_orientation:true,     //SET TO FALSE IF YOU DON'T WANT TO SHOW ORIENTATION ALERT ON MOBILE DEVICES
+        check_orientation:true,     
     })});
 
 function resizeCanvas() {
